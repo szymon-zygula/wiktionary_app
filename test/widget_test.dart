9562eb2b635936550eb20011edc153cb134d6995
Szymon Zygula <szymon@zygula.eu>
2022-01-08 20:24:35 +0100

    Add a basic test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,18 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:wiktionary_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const WiktionaryApp());
+  testWidgets('Test app creation', (WidgetTester tester) async {
+    WidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    await tester.pumpWidget(WiktionaryApp(Get.put(localStorage)));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.image(const AssetImage('assets/logo.png')), findsOneWidget);
   });
 }
